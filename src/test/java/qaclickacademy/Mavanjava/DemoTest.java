a20ab8575d6f7b09a4cfa17fa8039360886ae8f0
VJ <vincentj.dy@gmail.com>
2020-07-21 02:47:28 +0400

    First Commit

---
+++ src/test/java/qaclickacademy/Mavanjava/DemoTest.java
@@ -0,0 +1,10 @@
+package qaclickacademy.Mavanjava;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void demo() {
+		System.out.println("not to be executed!!");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavanjava/RestAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavanjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postsJira() {
+		System.out.println("postsJira");
+	}
+	
+	@Test
+	public void deleteTweet() {
+		System.out.println("deleteTweet");
+	}
+}
